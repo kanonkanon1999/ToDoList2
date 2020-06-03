de1f5490f23cabd09f95db22e920c1fd51e9b7ad
kanonkanon1999 <Konnomakoto713@icloud.com>
2020-06-03 23:51:33 +0900

    2020.6.3

--- Components/ToDoListItem.js
+++ Components/ToDoListItem.js
@@ -1,19 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Footer from './Footer';
+
+const {width} = Dimensions.get('window');
 
 export default function ToDoListItem() {
+    state = {
+        todoList: [],
+    };
+    addTodo = text => {
+        const list =[].concat(...this.state.todoList);
+        list.push({
+            key: Data.now(),
+            name: text,
+            isDone: false,
+        });
+        this.setState({
+            todoList:list,
+        });
+    };
+    handleCheck = index => {
+        const todos = [].concat(this.state.todoList);
+        todos[index].isDone = !todos[index].isDone;
+        this.setState({
+            todoList: todos,
+        });
+    }
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.ToDoListItemContainer}>
+        <FlatList
+            data={this.state.todoList}
+            renderItem={({item,index}) => {
+                return(
+                   <View>
+                        <Icon
+                          name={
+                            item.isDone ? 'checkbox-marked' : 'checkbox-blank-outline'   
+                          }
+                          isDone={item.isDone}
+                          style={styles.Checkbox}
+                          size={35}
+                          onPress={() => {
+                              this.handleCheck(index);
+                          }}
+                        />
+                        <Text>
+                            
+                        </Text>
+                    </View>
+                );
+            }}
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
